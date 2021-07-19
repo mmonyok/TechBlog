3c0ccc58cb3c7a830d4ca7c25e7b7e7eb878de59
Ody <melodymonyok@gmail.com>
2021-07-19 15:50:42 -0500

    Added session connection code.

--- server.js
+++ server.js
@@ -1,10 +1,35 @@
 const path = require('path');
 const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
 const routes = require('./controllers');
+const helpers = require('./utils/helpers');
+require('dotenv').config();
+
 const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const hbs = exphbs.create({ helpers })
+
+const sess = {
+  secret: process.env.SECRET,
+  cookie: {
+    maxAge: 900000,
+  },
+  resave: false,
+  saveUninitialized: false,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
