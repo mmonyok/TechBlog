bf9a0794c8f7c68567ab9b5a3e1c427d8758fdd2
Ody <melodymonyok@gmail.com>
2021-07-19 16:49:02 -0500

    Added association between user and blog.

---
+++ models/index.js
@@ -0,0 +1,13 @@
+const User = require('./User');
+const Blog = require('./Blog');
+
+User.hasMany(Blog, {
+  foreignKey: 'userId',
+  onDelete: 'CASCADE'
+});
+
+Blog.belongsTo(User, {
+  foreignKey: 'userId'
+});
+
+module.exports = { User, Blog };
