5cbff9158e8bc2bb779848a61cfc21f970177cf0
Ody <melodymonyok@gmail.com>
2021-07-21 19:11:20 -0500

    Updated my connection data to remove some uneccesary commented out stuff.

--- config/connection.js
+++ config/connection.js
@@ -1,23 +1,14 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-// Need to setup JAWSDB_URL on Heroku?
+// Need to setup JAWSDB_URL on Heroku
 
-/* let sequelize;
-
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-} */
-let sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
-  {
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
     host: process.env.DB_HOST,
     dialect: 'mysql',
     port: 3306
-  }
-);
+  });
 
 module.exports = sequelize;
