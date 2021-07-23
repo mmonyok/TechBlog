04f1deb8b0483631c07d3fab0898ce9f47f01efe
Ody <melodymonyok@gmail.com>
2021-07-23 13:42:52 -0500

    Updating connection data for JawsDB.

--- config/connection.js
+++ config/connection.js
@@ -9,7 +9,7 @@ if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
-    process.env.DB_PW,
+    process.env.DB_PASS,
     {
       host: 'localhost',
       dialect: 'mysql',
